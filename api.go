75f91c94038c76aeaef405f499add8fafbf2df83
Alexander Sheiko <alexander.sheiko@mgid.com>
2021-03-09 10:51:51 +0200

    Embed legacy user type

--- api.go
+++ api.go
@@ -15,7 +15,31 @@ type user struct {
 	Data struct {
 		User struct {
 			RestID string `json:"rest_id"`
-			Legacy User   `json:"legacy"`
+			Legacy struct {
+				CreatedAt   string `json:"created_at"`
+				Description string `json:"description"`
+				Entities    struct {
+					URL struct {
+						Urls []struct {
+							ExpandedURL string `json:"expanded_url"`
+						} `json:"urls"`
+					} `json:"url"`
+				} `json:"entities"`
+				FavouritesCount      int      `json:"favourites_count"`
+				FollowersCount       int      `json:"followers_count"`
+				FriendsCount         int      `json:"friends_count"`
+				IDStr                string   `json:"id_str"`
+				ListedCount          int      `json:"listed_count"`
+				Name                 string   `json:"name"`
+				Location             string   `json:"location"`
+				PinnedTweetIdsStr    []string `json:"pinned_tweet_ids_str"`
+				ProfileBannerURL     string   `json:"profile_banner_url"`
+				ProfileImageURLHTTPS string   `json:"profile_image_url_https"`
+				Protected            bool     `json:"protected"`
+				ScreenName           string   `json:"screen_name"`
+				StatusesCount        int      `json:"statuses_count"`
+				Verified             bool     `json:"verified"`
+			} `json:"legacy"`
 		} `json:"user"`
 	} `json:"data"`
 	Errors []struct {
--- types.go
+++ types.go
@@ -39,33 +39,6 @@ type (
 		Error error
 	}
 
-	// User type.
-	User struct {
-		CreatedAt   string `json:"created_at"`
-		Description string `json:"description"`
-		Entities    struct {
-			URL struct {
-				Urls []struct {
-					ExpandedURL string `json:"expanded_url"`
-				} `json:"urls"`
-			} `json:"url"`
-		} `json:"entities"`
-		FavouritesCount      int      `json:"favourites_count"`
-		FollowersCount       int      `json:"followers_count"`
-		FriendsCount         int      `json:"friends_count"`
-		IDStr                string   `json:"id_str"`
-		ListedCount          int      `json:"listed_count"`
-		Name                 string   `json:"name"`
-		Location             string   `json:"location"`
-		PinnedTweetIdsStr    []string `json:"pinned_tweet_ids_str"`
-		ProfileBannerURL     string   `json:"profile_banner_url"`
-		ProfileImageURLHTTPS string   `json:"profile_image_url_https"`
-		Protected            bool     `json:"protected"`
-		ScreenName           string   `json:"screen_name"`
-		StatusesCount        int      `json:"statuses_count"`
-		Verified             bool     `json:"verified"`
-	}
-
 	// timeline JSON
 	timeline struct {
 		GlobalObjects struct {
